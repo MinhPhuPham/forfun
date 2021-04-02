083964005d0f28ca052f2f936c79e93585722f58
knyL <linhleduc.it@hotmail.com>
2021-04-02 14:35:20 +0700

    Move header.component.scss to assets/themes/_header.scss

--- Web/src/app/layout/header/header.component.ts
+++ Web/src/app/layout/header/header.component.ts
@@ -3,15 +3,13 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss'],
+  //styleUrls: ['src/assets/scss/themes/_header.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HeaderComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   right = '-300';
   iconOwners = 'fa-plus';
@@ -21,35 +19,30 @@ export class HeaderComponent implements OnInit {
   isTenantsShow = false;
   isAboutShow = false;
 
-  ControlMobileMenu(isShow: boolean) : void {
-    this.right = isShow ? "0" : '-300';
+  ControlMobileMenu(isShow: boolean): void {
+    this.right = isShow ? '0' : '-300';
   }
 
-  ControlMobileSubMenu(isShow : boolean, subMenu : string) : void {
-    if(isShow) {
-      if(subMenu == 'owners') {
+  ControlMobileSubMenu(isShow: boolean, subMenu: string): void {
+    if (isShow) {
+      if (subMenu == 'owners') {
         this.iconOwners = 'fa-plus';
         this.isOwnersShow = !this.isOwnersShow;
-      }
-      else if(subMenu == 'tenants') {
+      } else if (subMenu == 'tenants') {
         this.iconTenants = 'fa-plus';
         this.isTenantsShow = !this.isTenantsShow;
-      }
-      else if(subMenu == 'about') {
+      } else if (subMenu == 'about') {
         this.iconAbout = 'fa-plus';
         this.isAboutShow = !this.isAboutShow;
       }
-    }
-    else {
-      if(subMenu == 'owners') {
+    } else {
+      if (subMenu == 'owners') {
         this.iconOwners = 'fa-minus';
         this.isOwnersShow = !this.isOwnersShow;
-      }
-      else if(subMenu == 'tenants') {
+      } else if (subMenu == 'tenants') {
         this.iconTenants = 'fa-minus';
         this.isTenantsShow = !this.isTenantsShow;
-      }
-      else if(subMenu == 'about') {
+      } else if (subMenu == 'about') {
         this.iconAbout = 'fa-minus';
         this.isAboutShow = !this.isAboutShow;
       }
