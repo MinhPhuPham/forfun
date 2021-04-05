e5e28ecf248c269941a8841eaac9d47c3f9da7b2
tngo <thoaingo07@hotmail.com>
2021-04-06 06:28:52 +0700

    Change banner image

--- Web/src/app/shared/components/dumbs/banner/banner.component.ts
+++ Web/src/app/shared/components/dumbs/banner/banner.component.ts
@@ -9,9 +9,31 @@ export class BannerComponent implements OnInit {
 
   constructor() { }
 
-  @Input() backgroundImage: string = '/assets/images/home/cpm_banner_home.png';
+  @Input() backgroundImage: string = '/assets/images/home/cpm_banner_home.webp';
 
   ngOnInit(): void {
+    const brower = this.getBrowserName();
+    console.log("brower", brower)
+    if (brower === 'safari')
+      this.backgroundImage = '/assets/images/home/cpm_banner_home.jpf';
   }
-
+  public getBrowserName() {
+    const agent = window.navigator.userAgent.toLowerCase()
+    switch (true) {
+      case agent.indexOf('edge') > -1:
+        return 'edge';
+      case agent.indexOf('opr') > -1 && !!(<any>window).opr:
+        return 'opera';
+      case agent.indexOf('chrome') > -1 && !!(<any>window).chrome:
+        return 'chrome';
+      case agent.indexOf('trident') > -1:
+        return 'ie';
+      case agent.indexOf('firefox') > -1:
+        return 'firefox';
+      case agent.indexOf('safari') > -1:
+        return 'safari';
+      default:
+        return 'other';
+    }
+  }
 }
