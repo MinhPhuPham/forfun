1d56932b2f0a7bbbe27391188fbd0c60a3b38cb3
knyL <linhleduc.it@hotmail.com>
2021-04-12 09:47:42 +0700

    Remove duplicate import

--- Web/src/app/pages/about/about.module.ts
+++ Web/src/app/pages/about/about.module.ts
@@ -1,12 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { DumbsModule, SrcollableControlModule } from 'src/app/shared';
-import { DynamicFormComponent } from 'src/app/shared/components/dynamic-form/dynamic-form.component';
 import { AboutRoutingModule } from './about-routing.module';
 import { AboutComponent } from './about.component';
-
 
 @NgModule({
   declarations: [AboutComponent],
@@ -16,7 +14,7 @@ import { AboutComponent } from './about.component';
     HttpClientModule,
     AboutRoutingModule,
     ReactiveFormsModule,
-    SrcollableControlModule
+    SrcollableControlModule,
   ],
 })
 export class AboutModule {}
